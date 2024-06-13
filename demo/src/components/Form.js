95ab3551eaee78bc453ad8ebbb19d1f437abc156
m30a09nu2000 <manusparappattu@gmail.com>
2024-06-13 09:55:33 +0530

    file upload using normal form

--- demo/src/components/Form.js
+++ demo/src/components/Form.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form() {
-  return <div>
-    form
-  </div>;
+  const [formData, setFormData] = useState("");
+  const handleFile = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  return (
+    <div>
+      <form onSubmit={handleFile}>
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => setFormData(e.target.value)}
+        />
+        <button>Upload</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
